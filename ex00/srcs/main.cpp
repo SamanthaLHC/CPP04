c00b76c6c3f51119ea02bd0f9ed3f8b07c9455ee
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-02 16:24:36 +0100

    step one for ex00

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -2,6 +2,7 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
+#include "colors.h"
 
 int main()
 {
@@ -10,8 +11,11 @@ const Animal* doggo = new Dog();
 const Animal* matou = new Cat();
 std::cout << doggo->getType() << " " << std::endl;
 std::cout << matou->getType() << " " << std::endl;
+std::cout << BMAG << "waiting for cat sound : " << RES << std::endl;
 matou->makeSound(); //will output the cat sound!
+std::cout << BMAG << "waiting for doggo sound : " << RES << std::endl;
 doggo->makeSound();
+std::cout << BMAG << "waiting for animal sound : " << RES << std::endl;
 pet->makeSound();
 return 0;
 }
